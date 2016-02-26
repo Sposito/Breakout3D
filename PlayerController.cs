a8c7ca59627c0e210b26fdc80e0d0beffbfab4bb
Sposito <Sposito@users.noreply.github.com>
2016-02-26 01:05:22 -0300

    player controller additional comments

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,27 +3,28 @@
 public class PlayerController : MonoBehaviour {
 	//Get values from the singleton to avoid unnesessary static reads;
 	float bounderies = GameBuilder.horizontalBounderies;
-	float speed = GameBuilder.handleSpeed;
+	float speed = GameBuilder.handleSpeed; 
 
 	bool releaseBall = true;
 
 	void FixedUpdate () {
-		MoveHandle ();
-		ReleaseBall ();
+		MoveHandle (); //moves the handle
+		ReleaseBall ();//Releases the ball when space is pressed
 
 	}
 
 	/// <summary>Moves the players handle left and right using system input </summary>
 	void MoveHandle(){
 		float x = transform.position.x;
-		if (Mathf.Abs (x) <= bounderies)
+		
+		if (Mathf.Abs (x) <= bounderies)//checks if the handle is inside moveable area
 			transform.Translate (Input.GetAxis ("Horizontal") * (speed / 10), 0f, 0f);
 		else {
-			int modulo = (x > 0) ? 1 : -1;
+			int modulo = (x > 0) ? 1 : -1; //determines the side where "collision" heappened
 			transform.position = Vector3.right * (bounderies * modulo);
 		}
 	}
-	/// <summaryStart the game when spacebar is pressed </summary>
+	/// <summary>Start the game when spacebar is pressed </summary>
 	void ReleaseBall(){
 		if (releaseBall) {
 			if (Input.GetKeyDown (KeyCode.Space)) {
